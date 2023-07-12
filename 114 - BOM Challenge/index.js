dfa7e8a00d365660d8abfd4af24c2a40cefbe011
Karim Mohamed Aly (ECC TCH) <karim.aly@elsewedy.com>
2023-07-12 20:55:33 +0300

    BOM Challenge working is added

--- 114 - BOM Challenge/index.js
+++ 114 - BOM Challenge/index.js
@@ -1,41 +1,59 @@
-let btn = document.querySelector("[type='submit']");
-let inputField = document.querySelector("[type='text']");
-let tasksContainer = document.getElementsByClassName("tasksContainer")[0];
-let taskDeleteBtn = document.getElementsByClassName("delete")[0];
+
+// define tasks container
+let tasksContainer = document.querySelector('[class="tasksContainer"]');
+//define add task btn
+let addTaskBtn = document.querySelector('[value="Add Task"]');
+//define inputField
+let inputField = document.getElementsByClassName("input")[0];
+// define local memory
+const memory = {};
+
+let idChar = 'a';
+let idInt = '0';
+let id = idChar + idInt;
+
+if (localStorage.getItem("memory")){
+
+    // get memory
+    let obj = JSON.parse(localStorage.memory);
+    id = Object.keys(obj).reduce((a, b) => obj[a] > obj [b] ? a:b);
+    
+    //add task
+    Object.values(obj).forEach(createTask);
+}
+
+addTaskBtn.addEventListener("click", addSingleTask);
+document.body.addEventListener("keypress", (e)=> {if (e.key == 'Enter') {addSingleTask();}})
 
 
-btn.addEventListener("click", ()=> {
-    if (inputField.value != "") {
-        let taskText = document.createTextNode(inputField.value);
-        let taskDiv = document.createElement("div");
-        taskDiv.classList.add("task");
+// defining a funtion that create the tasks 
+function createTask(param) {
+    let taskDiv = document.createElement("div");
+    let taskTextDiv = document.createElement("div");
+    let taskText = document.createTextNode(param);
+    let delBtn = document.createElement("input");
 
-        let taskTextDiv = document.createElement("div");
-        taskTextDiv.appendChild(taskText);
-        taskTextDiv.classList.add("taskText");
-    
-        let taskDeleteBtn = document.createElement("input");
-        taskDeleteBtn.classList.add("delete");
-        taskDeleteBtn.setAttribute("type","submit");
-        taskDeleteBtn.setAttribute("value","delete");
-        taskDeleteBtn.addEventListener("click", ()=> {
-            let taskToDelete = taskDeleteBtn.parentElement;
-            while (taskToDelete.children.length > 0) {
-                taskToDelete.firstElementChild.remove();
-            }
-            taskToDelete.remove();
-        })
-    
-        taskDiv.appendChild(taskTextDiv);
-        taskDiv.appendChild(taskDeleteBtn);
-    
-        tasksContainer.appendChild(taskDiv);
-        inputField.value = ""; 
+    taskDiv.classList.add("task");
+    taskTextDiv.classList.add("taskText");
+    delBtn.setAttribute("type","submit");
+    delBtn.classList.add("delete");
+    delBtn.setAttribute("value","delete");
+
+    taskTextDiv.appendChild(taskText);
+    taskDiv.appendChild(taskTextDiv);
+    taskDiv.appendChild(delBtn);
+    tasksContainer.append(taskDiv);
+}
+
+// define a function that adds a single task and updates the memory accordingly
+
+function addSingleTask() {
+    if (inputField.value){
+        createTask(inputField.value);
+        // update memory    
+        idInt++;
+        id = idChar + idInt;
+        memory[id] = inputField.value;
+        localStorage.memory = JSON.stringify(memory);
     }
-
-    else {
-        alert("Please enter the task name.");
-    }
-
-});
-
+}
