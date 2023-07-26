d6edaad4d6414e61e0b2950de7b893785a73da38
Karim Mohamed Aly (ECC TCH) <karim.aly@elsewedy.com>
2023-07-26 20:50:29 +0300

    Destructring arrays lesson start

---
+++ JavaScript Bootcamp from lesson 115 to 122/1/index.js
@@ -0,0 +1,6 @@
+let myNumbers = [1, 2, 3, 4, 5];
+
+[a,,,,e] = myNumbers; // Write Your Destructuring Assignment Here
+
+console.log(a * e); // 5
+
---
+++ Test/index.js
@@ -0,0 +1,13 @@
+const user = {theName:"Osama", theAge: 39, theTitle: "Developer", theCountry: "Egypt"};
+
+({theName, theAge, theTitle, theCountry} = user);
+
+console.log(user.theName);
+console.log(user.theAge);
+console.log(user.theTitle);
+console.log(user.theCountry);
+
+console.log(theName);
+console.log(theAge);
+console.log(theTitle);
+console.log(theCountry);
